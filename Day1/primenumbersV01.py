6d84118303b1a9d4677f26fc673eada0a17faf45
Stephen Odongo Oieno <stevenodongo@gmail.com>
2017-04-10 19:49:55 +0300

    Added FizzBuzz Andelabs Project script

--- Day1/primenumbersV01.py
+++
@@ -1,8 +0,0 @@
-def prime_numbers (n):
-    for number in range (n):
-        for element in range (1,number):
-            if (number%element==0):
-                break
-            else:
-                return (element)
-    
--- Day3/wordcount.py
+++
@@ -1,5 +0,0 @@
-def words(sentence):
-	wordlist = sentence.split()
-	wordfrequency = [wordlist.count(w) for w in wordlist]
-	return dict(zip(wordlist, wordfrequency))
-
